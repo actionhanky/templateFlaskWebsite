9a16e49873520a7da5d3337df2e97f6e3f693b73
Sam Hields <samhields@sams-air.home>
2019-10-27 14:31:03 +0000

    theproject

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from puppycompanyblog import app
+from theproject import app
 
 if __name__ == '__main__':
     app.run(debug=True)
--- theproject/users/views.py
+++ theproject/users/views.py
@@ -1,10 +1,10 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
-from puppycompanyblog import db
+from theproject import db
 from werkzeug.security import generate_password_hash,check_password_hash
-from puppycompanyblog.models import User, BlogPost
-from puppycompanyblog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
-from puppycompanyblog.users.picture_handler import add_profile_pic
+from theproject.models import User, BlogPost
+from theproject.users.forms import RegistrationForm, LoginForm, UpdateUserForm
+from theproject.users.picture_handler import add_profile_pic
 
 
 users = Blueprint('users', __name__)
